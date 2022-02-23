00a32dc1f2fed8317fdeb8a8936b02a53f6fb4b1
Igor Biondo <igorbiondo@gmail.com>
2022-02-23 12:01:19 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById('play').addEventListener('click', () => {
+    let gameUrl = './Spmw/smw.sfc'
+    location='./test.html' 
+
+});
+
+var dados = 'dkc'
+sessionStorage.setItem('chave', dados );
